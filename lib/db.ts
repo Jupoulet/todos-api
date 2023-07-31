19041901b1d54bd865f4cc98a933cb16e6da64d5
Jupoulet <julien.pierre.picard@gmail.com>
2023-07-31 14:27:36 +0200

    remove unecessary console.log

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
-import postgres from 'postgres'
-console.log('PROCESS.ENV', process.env.DATABASE_URL);
+import postgres from 'postgres';
+
 const sql = postgres(process.env.DATABASE_URL as string, {
   ssl: 'require'
-})
+});
 
-export default sql
+export default sql;
